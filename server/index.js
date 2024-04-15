700c907bdfecd75e36a17a17294e1214ce60900a
Anideep <anideepkalia@gmail.com>
2024-04-15 19:33:32 +0530

    index.js cahnges

--- server/index.js
+++ server/index.js
@@ -1,29 +1,31 @@
-const { ApolloServer, PubSub } = require('apollo-server');
-const mongoose = require('mongoose');
-
+const { ApolloServer, PubSub } = require('apollo-server-express');
+const express = require('express');
+const { dbConnection } = require('./db');
+const cors = require('cors');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
-const { MONGODB } = require('./config.js');
 
 const pubsub = new PubSub();
+const PORT = 5000;
 
-const PORT = process.env.port || 5000;
-
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({ req, pubsub })
+  context: ({ req }) => ({ req, pubsub }),
 });
 
-mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
-  .then(() => {
-    console.log('MongoDB Connected');
-    return server.listen({ port: PORT });
-  })
-  .then((res) => {
-    console.log(`Server running at ${res.url}`);
-  })
-  .catch(err => {
-    console.error(err)
-  })
+server.applyMiddleware({ app });
+
+dbConnection();
+
+app.use(express.json());
+app.use(cors());
+
+// app.use('/api/user', require('./routes/user'));
+// app.use('/api/admin', require('./routes/admin'));
+// app.use('/api/place', require('./routes/place'));
+
+app.listen(PORT, () => {
+  console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+});

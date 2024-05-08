d626525fae1cca4e4acaf32c981e250c03ca90d3
Anideep <anideepkalia@gmail.com>
2024-05-08 23:40:46 +0530

    thi si today

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-exports.dbConnection =async()=>{
+exports.dbConnection = async() => {
     mongoose.set('strictQuery',true)
     await mongoose.connect('mongodb+srv://anideepkalia:anideepkalia@cluster0.d9ep4jo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
         .then(()=>{
